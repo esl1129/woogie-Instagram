d9992f91717c040e9f906f2c89fdda54d60720fb
esl1129 <esl112999@gmail.com>
2021-09-30 15:16:22 +0900

    Add Profile Edit Page On SettingsViewController

---
+++ woogie-Instagram/Views/Cells/Home Feed/IGFeedPostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  IGFeedPostTableViewCell.swift
+//  woogie-Instagram
+//
+//  Created by 임재욱 on 2021/09/30.
+//
+
+import UIKit
+
+final class IGFeedPostTableViewCell: UITableViewCell {
+
+    static let identifier = "IGFeedPostTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(){
+        // configure the cell
+    }
+}
